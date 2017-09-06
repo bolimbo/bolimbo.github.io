5dfd1b1a9e78deb08541d4f3b1bbb6566ad533b1
Bolimbo <carlo.cappellini@yahoo.com>
2017-09-06 09:03:29 -0500

    added jquery ui js, jquery ui css and ajax, use .dialog to display gif

--- js/my_website.js
+++ js/my_website.js
@@ -3,13 +3,30 @@
 
 
 $(".boton").on("click", function () {
+    $(".h2").css("color", "gray")
+    $(".h2").css("font-size", "70px")
 
-    $(".h2").html("THE FORCE IS WITH ME......").fadeToggle(6000)
+    $(".h2").html("THE FORCE IS WITH ME......").fadeToggle(6000, .07)
 
 
+
+});
+
+$(".top").mouseover(function () {
+    $(this).fadeIn();
 
 })
+//
+//
 
+$("h1").on("click", function () {
+    $("")
+    $("#star-wars").show()
+    $(this).hide()
+})
+$( function() {
+    $( "#dialog" ).dialog();
+} );
 
 
 
